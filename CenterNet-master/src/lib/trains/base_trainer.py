e8d3afe8f9df75ddacd63a7149215264a1275554
JiwonCocoder <45135820+JiwonCocoder@users.noreply.github.com>
2020-06-08 18:08:04 +0900

    Add files via upload
    trains

--- CenterNet-master/src/lib/trains/base_trainer.py
+++ CenterNet-master/src/lib/trains/base_trainer.py
@@ -7,6 +7,9 @@ import torch
 from progress.bar import Bar
 from models.data_parallel import DataParallel
 from utils.utils import AverageMeter
+import visdom
+import numpy as np
+
 
 
 class ModleWithLoss(torch.nn.Module):
